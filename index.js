6b9dc78098d6641e41291cc1dec7856047e5b6ab
dn.guido <guido.dn59@gmail.com>
2022-06-29 17:49:06 -0300

    bucles y condicionales

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-for (let i = 0; i < 1; i++){
-    let nombre = prompt("Ingresa tu nombre");
-    let mensaje = (`Bienvenido ${nombre}`)
-    alert (mensaje)
+let usuario = prompt ("Ingrese un usuario")
+
+while (usuario != "guido"){
+    alert ("Usuario incorrecto")
+    usuario = prompt ("Ingrese un usuario")
+}if (usuario === "guido"){
+    alert ("Bienvenido")
 }
+
+
+
+
+
+
+
+
+
+
+
+/* Practicando Switch
+let edad = prompt ("Cuál es tu edad")
+
+switch (edad){
+    case "0":
+        console.log ("Tenes cero años")
+        break;
+
+    case "10":
+        console.log ("Tenes 10 años")
+        break;
+
+    case "50":
+        console.log ("Tenes 50 años")
+        break;
+
+    default:
+        console.log("No tenes ni 0, ni 10, ni 50 años")
+        break;
+
+}
+ */
+
+
