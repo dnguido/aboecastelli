c7eb0cda70b4e5cc32f7f30b4ec6bb16a1d05d6e
dn.guido <guido.dn59@gmail.com>
2022-07-12 19:12:22 -0300

    arrays

--- index.js
+++ index.js
@@ -9,6 +9,42 @@ class Producto {
     } 
 } 
  
+const productos = { 
+    impresion3D: [ 
+        {nombre: "mate 3d", precio: 1000}, 
+        {nombre: "taza 3d", precio: 1500} 
+    ], 
+    decoracion:[ 
+        {nombre: "tapiz macrame", precio: 1200}, 
+        {nombre: "espejo macrame", precio: 1800} 
+    ] 
+} 
+ 
+ 
+ 
+for (const item of productos){ 
+    if (item.nombre == "taza 3d"){ 
+        item.vender(); 
+    } 
+} 
+ 
+console.log(productos);
+
+
+
+
+
+/* class Producto { 
+    constructor (nombre, precio){ 
+        this.nombre = nombre; 
+        this.precio = precio; 
+        this.vendido = false; 
+    } 
+    vender(){ 
+        this.vendido = true; 
+    } 
+} 
+ 
 let productos = []; 
  
 productos.push(new Producto ("mate 3d", 1000)); 
@@ -26,16 +62,7 @@ for (const item of productos){
     }
 
  }
-console.log(productos);
-
-
-
-
-
-
-
-
-
+console.log(productos); */
 
 
 
