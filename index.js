5c8f789dc185aa1c8278f6c7b547c9e5aff537f3
dn.guido <guido.dn59@gmail.com>
2022-07-14 13:45:27 -0300

    Primera entrega de proyecto final

--- index.js
+++ index.js
@@ -1,77 +1,93 @@
-class Producto { 
-    constructor (nombre, precio){ 
-        this.nombre = nombre; 
-        this.precio = precio; 
-        this.vendido = false; 
-    } 
-    vender(){ 
-        this.vendido = true; 
-    } 
-} 
- 
-const productos = { 
-    impresion3D: [ 
-        {nombre: "mate 3d", precio: 1000}, 
-        {nombre: "taza 3d", precio: 1500} 
-    ], 
-    decoracion:[ 
-        {nombre: "tapiz macrame", precio: 1200}, 
-        {nombre: "espejo macrame", precio: 1800} 
-    ] 
-} 
- 
- 
- 
-for (const item of productos){ 
-    if (item.nombre == "taza 3d"){ 
-        item.vender(); 
-    } 
-} 
- 
+/* suma total de una supuesta compra con funciones de orden superior */
+
+class stockCollection extends Array {
+    suma(clave) {
+        return this.reduce((a, b) => a + (b[clave] || 0), 0);
+    }
+}
+const stock = new stockCollection(...[
+    {  id: 'taza 3d', precio: 500},
+    {  id: 'mate 3d', precio: 150},
+    {  id: 'lampara 3d', precio: 130},
+    {  id: 'tapiz macrame', precio: 200},
+    {  id: 'espejo macrame', precio: 150},]);
+
+console.log(stock.suma('precio'));
+
+
+/* venta de productos */
+
+class Productos{
+    constructor (nombre, precio){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.vendido = false;
+    }
+
+    vender(){
+        this.vendido = true;
+    }
+}
+
+let productos = [];
+
+productos.push(new Productos ("taza 3d", 500))
+productos.push(new Productos ("mate 3d", 150))
+productos.push(new Productos ("lampara 3d", 130))
+productos.push(new Productos ("tapiz macrame", 200))
+productos.push(new Productos ("espejo macrame", 150))
+
+for (const item of productos){
+    if (item.nombre == "mate 3d" && "tapiz macrame"){
+        item.vender();
+    }
+}
+
 console.log(productos);
 
 
+/* precios acualizados stock con funciones de orden superior */
+
+stockActualizado = [
+    {  id: 'taza 3d', precio: 800},
+    {  id: 'mate 3d', precio: 200},
+    {  id: 'lampara 3d', precio: 130},
+    {  id: 'tapiz macrame', precio: 300},
+    {  id: 'espejo macrame', precio: 150},];
+   const nuevo = stockActualizado.map(item => item.precio).reduce((prev, curr) => prev + curr, 0);
+   console.log(nuevo);
 
 
 
-/* class Producto { 
-    constructor (nombre, precio){ 
-        this.nombre = nombre; 
-        this.precio = precio; 
-        this.vendido = false; 
-    } 
-    vender(){ 
-        this.vendido = true; 
-    } 
-} 
- 
-let productos = []; 
- 
-productos.push(new Producto ("mate 3d", 1000)); 
-productos.push(new Producto ("personaje 3d", 2000)); 
-productos.push(new Producto ("lampara 3d", 2500)); 
-productos.push(new Producto ("taza 3d", 1500)); 
-productos.push(new Producto ("tapiz macrame", 1200)); 
-productos.push(new Producto ("espejo macrame", 1800)); 
- 
-for (const item of productos){ 
-    if (item.nombre == "lampara 3d"){ 
-        item.vender(); 
-    }else if(item.nombre == "taza 3d"){
-        item.vender();
+
+
+
+
+
+
+
+
+
+
+
+
+/* class productosColecction{
+    suma(clave){
+        return this.reduce((a, b) => a (b [clave] || 0), 0) 
     }
+}
 
- }
-console.log(productos); */
+const productos = new productosColecction(... [
+    {id: "taza 3d", precio: 1500},
+    {id: "mate 3d", precio: 1000},
+    {id: "lampara 3d", precio: 2000},
+    {id: "personaje 3d", precio: 800},
+    {id: "espejo macrame", precio: 1800},
+    {id: "tapiz macrame", precio: 1200},]);
 
-
-
-
-
-
-
-
-
+console.log(productos.suma("precio"));        
+  */
+ 
 
 
 
